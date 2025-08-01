cb9ea9d20f60ae8f19d9758f83e75e4140135aec
Mathumithabalakrishnan <mathumithabalakrishnan11@gmail.com>
2025-08-01 22:31:56 +0530

    Add files via upload

---
+++ guessnum.py
@@ -0,0 +1,28 @@
+import random
+
+def guess_the_number():
+    # Generate a random number between 1 and 100
+    number_to_guess = random.randint(1, 100)
+    attempts = 0
+
+    print("Welcome to the Number Guessing Game!")
+    print("I have selected a number between 1 and 100. Can you guess it?")
+
+    while True:
+        try:
+            user_guess = int(input("Enter your guess: "))
+            attempts += 1
+
+            if user_guess < number_to_guess:
+                print("Too low! Try again.")
+            elif user_guess > number_to_guess:
+                print("Too high! Try again.")
+            else:
+                print(f"Congratulations! You guessed the number {number_to_guess} in {attempts} attempts.")
+                break
+
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+# Run the game
+guess_the_number()
